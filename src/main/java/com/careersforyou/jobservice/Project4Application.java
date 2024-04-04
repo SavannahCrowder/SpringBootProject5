53414b781e0c5a3aefbe379752874024ebb8eb09
Savannah Crowder <sdcrow@yahoo.com>
2024-04-04 16:23:36 -0700

    Initial Commit

--- src/main/java/com/careersforyou/jobservice/Project4Application.java
+++ src/main/java/com/careersforyou/jobservice/Project4Application.java
@@ -1,9 +1,14 @@
 package com.careersforyou.jobservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Project4Application {
 
     public static void main(String[] args) {
